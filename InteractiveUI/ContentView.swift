fe0e550ac7267d315a0fa40f366f062ce7d5397c
Sayuri Bose <sayuribose@gmail.com>
2025-08-06 14:53:07 -0400

    changed the texts and added @ state, made the button and textbox usable.

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
-            TextField("Type Name Here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type Name Here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
             Button("Submit Name") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                
+                textTitle = "Welcome \(name)!"
+                name = ""
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
