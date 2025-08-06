10d3b3dba4e07cdd3f01429275592d5b2dd40e12
Sayuri Bose <sayuribose@gmail.com>
2025-08-06 15:12:31 -0400

    added alert and extra input box and age and title age

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,19 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     @State private var name = ""
-    @State private var textTitle = "What is your name?"
+    @State private var age = ""
+    @State private var textTitle = "What is your name and age?"
     var body: some View {
         VStack {
+            
+            
             Text(textTitle)
                 .font(.title)
             TextField("Type Name Here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
+            TextField("Type Age Here...", text: $age)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
             Button("Submit Name") {
-                textTitle = "Welcome \(name)!"
+                textTitle = "Welcome \(name)! You are \(age) years old."
                 name = ""
+                    showingAlert = true
+                
+            }
+            .alert("Are you sure?", isPresented: $showingAlert){
+                Button("Yes", role: .cancel){}
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
